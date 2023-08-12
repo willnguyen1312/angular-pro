dd8d618b4ff5188386c582dabe578e026dc2d125
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-08-12 09:30:11 -0300

    xD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,144 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core"
+import {
+  ZoomImageClickService,
+  ZoomImageHoverService,
+  ZoomImageMoveService,
+  ZoomImageWheelService,
+} from "@zoom-image/angular"
+import {
+  ZoomImageClickState,
+  ZoomImageHoverState,
+  ZoomImageMoveState,
+  ZoomImageWheelState,
+  cropImage,
+} from "@zoom-image/core"
+
+type Tab = {
+  name: string
+  href: string
+  current: boolean
+  value: "wheel" | "hover" | "move" | "click"
+}
+
+type ZoomType = "wheel" | "hover" | "move" | "click"
+
+const sleep = (ms = 0) => new Promise((resolve) => setTimeout(resolve, ms))
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  providers: [ZoomImageClickService, ZoomImageHoverService, ZoomImageMoveService, ZoomImageWheelService],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild("imageWheelContainer") imageWheelContainerRef?: ElementRef<HTMLDivElement>
+  @ViewChild("imageHoverContainer") imageHoverContainerRef?: ElementRef<HTMLDivElement>
+  @ViewChild("imageMoveContainer") imageMoveContainerRef?: ElementRef<HTMLDivElement>
+  @ViewChild("imageClickContainer") imageClickContainerRef?: ElementRef<HTMLDivElement>
+  @ViewChild("zoomTarget") zoomTargetRef?: ElementRef<HTMLDivElement>
+
+  tabs: Tab[] = [
+    { name: "Wheel", href: "#", current: true, value: "wheel" },
+    { name: "Hover", href: "#", current: false, value: "hover" },
+    { name: "Move", href: "#", current: false, value: "move" },
+    { name: "Click", href: "#", current: false, value: "click" },
+  ]
+
+  zoomType: ZoomType = "wheel"
+  croppedImage: string = ""
+
+  zoomImageWheelState: ZoomImageWheelState = this.zoomImageWheelService.zoomImageState
+  zoomImageHoverState: ZoomImageHoverState = this.zoomImageHoverService.zoomImageState
+  zoomImageMoveState: ZoomImageMoveState = this.zoomImageMoveService.zoomImageState
+  zoomImageClickState: ZoomImageClickState = this.zoomImageClickService.zoomImageState
+
+  constructor(
+    private zoomImageWheelService: ZoomImageWheelService,
+    private zoomImageHoverService: ZoomImageHoverService,
+    private zoomImageMoveService: ZoomImageMoveService,
+    private zoomImageClickService: ZoomImageClickService,
+  ) { }
+
+  ngAfterViewInit(): void {
+    if (this.imageWheelContainerRef) {
+      this.zoomImageWheelService.createZoomImage(this.imageWheelContainerRef.nativeElement)
+      this.zoomImageWheelService.zoomImageState$.subscribe((state: any) => {
+        this.zoomImageWheelState = state
+      })
+    }
+  }
+
+  getCurrentZoomImageValue() {
+    return `${Math.round(this.zoomImageWheelState.currentZoom * 100)}%`
+  }
+
+  async handleTabClick(tab: Tab) {
+    const zoomType = tab.value
+    if (zoomType === this.zoomType) {
+      return
+    }
+
+    this.croppedImage = ""
+    this.tabs.forEach((tab) => (tab.current = false))
+    tab.current = true
+    this.zoomType = zoomType
+
+    await sleep()
+
+    const handlers: Record<ZoomType, () => void> = {
+      wheel: () => {
+        this.zoomImageWheelService.createZoomImage(this.imageWheelContainerRef?.nativeElement as HTMLDivElement)
+        this.zoomImageWheelService.zoomImageState$.subscribe((state: any) => {
+          this.zoomImageWheelState = state
+          this.handleCropWheelZoomImage()
+        })
+      },
+      hover: () => {
+        this.zoomImageHoverService.createZoomImage(this.imageHoverContainerRef?.nativeElement as HTMLDivElement, {
+          zoomImageSource: "https://nam-assets.netlify.app/static/large.webp",
+          customZoom: { width: 300, height: 500 },
+          zoomTarget: this.zoomTargetRef?.nativeElement as HTMLDivElement,
+          scaleFactor: 0.5,
+        })
+        this.zoomImageHoverService.zoomImageState$.subscribe((state: any) => {
+          this.zoomImageHoverState = state
+        })
+      },
+      move: () => {
+        this.zoomImageMoveService.createZoomImage(this.imageMoveContainerRef?.nativeElement as HTMLDivElement, {
+          zoomImageSource: "https://nam-assets.netlify.app/static/large.webp",
+        })
+        this.zoomImageMoveService.zoomImageState$.subscribe((state: any) => {
+          this.zoomImageMoveState = state
+        })
+      },
+      click: () => {
+        this.zoomImageClickService.createZoomImage(this.imageClickContainerRef?.nativeElement as HTMLDivElement, {
+          zoomImageSource: "https://nam-assets.netlify.app/static/large.webp",
+        })
+        this.zoomImageClickService.zoomImageState$.subscribe((state: any) => {
+          this.zoomImageClickState = state
+        })
+      },
+    }
+
+    handlers[zoomType]()
+  }
+
+  zoomIn() {
+    this.zoomImageWheelService.setZoomImageState({ currentZoom: this.zoomImageWheelState.currentZoom + 0.5 })
+  }
+
+  zoomOut() {
+    this.zoomImageWheelService.setZoomImageState({ currentZoom: this.zoomImageWheelState.currentZoom - 0.5 })
+  }
+
+  handleCropWheelZoomImage() {
+    this.croppedImage = cropImage({
+      currentZoom: this.zoomImageWheelState.currentZoom,
+      image: (this.imageWheelContainerRef?.nativeElement as HTMLDivElement).querySelector("img") as HTMLImageElement,
+      positionX: this.zoomImageWheelState.currentPositionX,
+      positionY: this.zoomImageWheelState.currentPositionY,
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,12 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core"
+import { BrowserModule } from "@angular/platform-browser"
 
-import { AppComponent } from './app.component';
-import { CartComponent } from './cart/cart.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ShippingComponent } from './shipping/shipping.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
+import { AppComponent } from "./app.component"
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent },
-      { path: 'shipping', component: ShippingComponent },
-    ])
-  ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent,
-    ProductDetailsComponent,
-    CartComponent,
-    ShippingComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
